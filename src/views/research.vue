449becda8e9e81ab12afbe434896ae8e1bdeff7a
thekozo <99541970+thekozo@users.noreply.github.com>
2023-03-10 15:49:42 +0700

    Update research.vue

--- src/views/research.vue
+++ src/views/research.vue
@@ -5,7 +5,9 @@
             width="100%"
             height="1000px"
             scrolling="no"
-            frameborder="0">
+            frameborder="0"
+            sandbox="allow-same-origin allow-scripts">
             </iframe>
   </div>
 </template>
+<iframe src="https://example.com" sandbox="allow-same-origin allow-scripts"></iframe>
